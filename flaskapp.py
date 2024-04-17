28bd6b646d07bfb23f5703ea014cf84bd98f0523
MihaiB-dev <mihaibivol.dev@gmail.com>
2024-04-17 07:42:49 +0300

    change the flaskapp other template

--- flaskapp.py
+++ flaskapp.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-  return "Hello, World!"
+	return 'Hello World Again'
 
-if __name__ == "__main__":
-  app.run(debug=True)
+# main driver function
+if __name__ == '__main__':
+	app.run()
